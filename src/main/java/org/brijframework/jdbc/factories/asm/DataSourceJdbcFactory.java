f4f0af332f6f265cd78a880e29da66a004828b09
ram kishor <ramkishor0509@gmail.com>
2019-06-19 21:34:45 +0530

    ok

--- src/main/java/org/brijframework/jdbc/factories/asm/DataSourceJdbcFactory.java
+++ src/main/java/org/brijframework/jdbc/factories/asm/DataSourceJdbcFactory.java
@@ -8,11 +8,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.sql.DataSource;
 
 import org.brijframework.container.Container;
-import org.brijframework.data.factories.asm.ClassDataSetupFactoryImpl;
-import org.brijframework.data.setup.ClassDataSetup;
+import org.brijframework.data.factories.asm.ClassDataInfoFactoryImpl;
+import org.brijframework.data.info.ClassDataInfo;
 import org.brijframework.jdbc.factories.JdbcFactory;
-import org.brijframework.meta.factories.asm.MetaSetupFactoryImpl;
-import org.brijframework.meta.info.ClassMetaInfo;
 import org.brijframework.model.factories.ModelFactory;
 import org.brijframework.support.model.Assignable;
 import org.brijframework.util.asserts.Assertion;
@@ -45,15 +43,12 @@ public class DataSourceJdbcFactory implements JdbcFactory{
 
 	@Override
 	public DataSourceJdbcFactory loadFactory() {
-		List<ClassMetaInfo> owners= MetaSetupFactoryImpl.getFactory().getMetaList(DataSource.class);
-		for(ClassMetaInfo owner:owners) {
-			List<ClassDataSetup> dataList= ClassDataSetupFactoryImpl.getFactory().getSetupList(owner.getId());
-			for(ClassDataSetup datainfo:dataList) {
-				DataSource dataSource=ModelFactory.getFactory().getModel(owner,datainfo);
-				if(dataSource!=null) {
-					getCache().put(datainfo.getId(), dataSource);
-				}
-			}
+		List<ClassDataInfo> owners= ClassDataInfoFactoryImpl.getFactory().getDataList(DataSource.class.getName());
+		for(ClassDataInfo owner:owners) {
+			DataSource dataSource=ModelFactory.getFactory().getModel(owner);
+			if(dataSource!=null) {
+				getCache().put(owner.getId(), dataSource);
+			}	
 		}
 		return this;
 	}
