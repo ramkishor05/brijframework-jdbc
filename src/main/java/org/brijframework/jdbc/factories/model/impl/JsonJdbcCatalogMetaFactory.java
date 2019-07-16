1d15b341437fca3ad1087d67d845bf7c622bc359
ramkishor05 <ramkishor05@gmail.com>
2019-07-16 09:59:53 +0530

    ok

---
+++ src/main/java/org/brijframework/jdbc/factories/model/impl/JsonJdbcCatalogMetaFactory.java
@@ -0,0 +1,54 @@
+package org.brijframework.jdbc.factories.model.impl;
+
+import java.io.File;
+
+import org.brijframework.asm.factories.AbstractFactory;
+import org.brijframework.jdbc.JdbcCatalog;
+import org.brijframework.jdbc.util.JdbcBeanUtil;
+import org.brijframework.util.location.StreamUtil;
+
+public abstract class JsonJdbcCatalogMetaFactory extends AbstractFactory<File,JdbcCatalog>{
+
+	public void build(File catalogFile, JdbcCatalog catalog) {
+		try {
+			catalog.makeCatalog();
+			catalog.getTables().forEach((tableKey,table)->{
+				try {
+					table.setCatalog(catalog);
+					if(table.getTableCat()==null) {
+						table.setTableCat(catalog.getTableCat());
+					}
+					JdbcBeanUtil.makeTable(table);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void upload(File catalogFile, JdbcCatalog catalog) {
+		try {
+			catalog.makeCatalog();
+			catalog.getTables().forEach((tableKey,table)->{
+				try {
+					table.setCatalog(catalog);
+					if(table.getTableCat()==null) {
+						table.setTableCat(catalog.getTableCat());
+					}
+					JdbcBeanUtil.makeTable(table);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void download(File catalogFile, JdbcCatalog catalog) {
+		StreamUtil.writeJsonToFile(catalogFile.toPath(), catalog);
+	}
+
+}
